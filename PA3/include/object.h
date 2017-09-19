50fec1265ddc741e8f223da3b64b61a3c49d3567
Brianna Blain-Castelli <bblaincastelli@nevada.unr.edu>
2017-09-19 09:39:59 -0700

    PA3 upload

---
+++ PA3/include/object.h
@@ -0,0 +1,33 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <vector>
+#include "graphics_headers.h"
+
+class Object
+{
+  public:
+    Object();
+    ~Object();
+    void Update(unsigned int dt);
+    void Update(unsigned int dt, Object cube);
+    void Render();
+    void setKeyT( char toSet);
+    void setKeyR( char toSet);
+
+    glm::mat4 GetModel();
+
+  private:
+    glm::mat4 model;
+    std::vector<Vertex> Vertices;
+    std::vector<unsigned int> Indices;
+    GLuint VB;
+    GLuint IB;
+
+    float angle;
+    float angleRotate;
+    char keypressT = 'f';
+    char keypressR = 'r';
+};
+
+#endif /* OBJECT_H */
